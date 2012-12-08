55b65709018f114fd516cfd7f9c971f0af9981d0
Arunram Kalaiselvan <arun@edengroups.com>
2012-12-08 14:58:14 -0500

    sign in form

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,12 @@
 class DashboardController < ApplicationController
   def index
   	@foo = 'bar boo';
+
+  	geoloqi_session = Geoloqi::Session.new(
+	  :access_token => "YOUR APPLICATION ACCESS TOKEN"
+	)
+
+	result = geoloqi_session.post("trigger/create", {
+	})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Myapp::Application.routes.draw do
-  get "dashboard/index"
+  devise_for :users
+  root :to => 'dashboard#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +51,6 @@ Myapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 
