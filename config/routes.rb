081f81ad3a60d4bba6d2941e90712c495a347e8b
Arunram Kalaiselvan <arun@edengroups.com>
2012-12-08 13:44:49 -0500

    Added in the root route and dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Myapp::Application.routes.draw do
+  get "dashboard/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Myapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 
