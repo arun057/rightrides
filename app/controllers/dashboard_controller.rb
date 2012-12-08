69cb680f4d356eed2c552da40a7f98bd054156ab
Arunram Kalaiselvan <arun@edengroups.com>
2012-12-08 15:10:41 -0500

    added in some seeds and signup finished

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,11 @@
 
 class DashboardController < ApplicationController
+  before_filter :authenticate_user!
   def index
   	@foo = 'bar boo';
 
-  	geoloqi_session = Geoloqi::Session.new(
-	  :access_token => "YOUR APPLICATION ACCESS TOKEN"
-	)
-
-	result = geoloqi_session.post("trigger/create", {
-	})
+ #  	geoloqi_session = Geoloqi::Session.new(
+	#   :access_token => "YOUR APPLICATION ACCESS TOKEN"
+	# )
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+	:email => 'dev@rightrides.org',
+	:password => 'rrhack388',
+	:password_confirmation => 'rrhack388'
+)
